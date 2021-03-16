c06814689e8f4b4c5a5c318c3c82c32f0c84b7ae
JaewonMin <mjw8523@gmail.com>
2021-03-16 20:41:30 +0900

    3/16

--- Hash_1.cpp
+++ Hash_1.cpp
@@ -1,19 +1,27 @@
+////
+//// Created by Macbook Pro on 2020/12/21.
+////
 //
-// Created by Macbook Pro on 2020/12/21.
+//#include <iostream>
 //
-#include <string>
-#include <vector>
-#include <iostream>
-#include <unordered_map>
-using namespace std;
-
-string solution(vector<string> participant, vector<string> completion) {
-
-}
-
-
-int main(){
-    vector<string> participant = {"leo","kiki","eden","kiki"};
-    vector<string> completion = {"eden","kiki","leo"};
-    cout << solution(participant,completion) << endl;
-}
+//using namespace std;
+//
+//int answer = 0;
+//
+//void fun(int a,int b,int c,int n,int sum){
+//    if(sum == n) answer = 1;
+//    if(answer == 1 || sum > n) return;
+//    fun(a,b,c,n,sum+a);
+//    fun(a,b,c,n,sum+b);
+//    fun(a,b,c,n,sum+c);
+//}
+//
+//int main(){
+////
+//    int a,b,c,n;
+//    cin >> a >> b >> c >> n;
+//
+//    fun(a,b,c,n,0);
+//    cout << answer;
+//
+//}
--- This_is_codding_test/greedy_1.py
+++ This_is_codding_test/greedy_1.py
@@ -1,13 +1,30 @@
+#
+# n, k = map(int, input().split())
+# answer = 0
+# while(n != 1):
+#     target = (n // k) * k
+#     answer += (n - target)
+#     n = target
+#     if n < k:
+#         break
+#     answer += 1
+#     n //= k
+# answer += (n-1)
+# print(answer)
 
-n, k = map(int, input().split())
+def fun(a,b,c,n,sum):
+    if sum == n:
+        answer = 1
+    if answer == 1 or sum > n:
+        return
+    fun(a,b,c,n,sum+a)
+    fun(a,b,c,n,sum+b)
+    fun(a,b,c,n,sum+c)
+
+
+a,b,c,n = map(int, input().split())
+
 answer = 0
-while(n != 1):
-    target = (n // k) * k
-    answer += (n - target)
-    n = target
-    if n < k:
-        break
-    answer += 1
-    n //= k
-answer += (n-1)
+fun(a,b,c,n,0)
+
 print(answer)
