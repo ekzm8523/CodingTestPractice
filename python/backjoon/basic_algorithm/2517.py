748e4ea51213092cad40b784010abcb24b06b852
민재원 <jaewon@minjaewon-ui-Macmini.local>
2021-08-26 10:23:59 +0900

    8/26

--- python/backjoon/basic_algorithm/2517.py
+++ python/backjoon/basic_algorithm/2517.py
@@ -13,13 +13,25 @@ node -> 2 ** h - 1
 """
 import sys
 from math import log2, ceil
+import os
 
 def insert(num):
     global start_leaf
-    leaf = start_leaf + num - 1
+    leaf = start_leaf + num
     while leaf > 0:
-        tree[leaf] += num
+        tree[leaf] += 1
         leaf //= 2
+
+def prefix_sum(start, end, node, num):
+
+    if start > num:
+        return 0
+    if num >= end:
+        return tree[node]
+    elif num < end:
+        mid = (start + end) // 2
+        return prefix_sum(start, mid, node * 2, num) + prefix_sum(mid + 1, end, node * 2 + 1, num)
+
 
 def print_tree():
     for i in range(1, h+1):
@@ -37,42 +49,22 @@ if __name__ == "__main__":
         runners[s] = i
 
     for rank, key in enumerate(sorted(runners)):
-        runners[key] = rank
+        runners[key] = rank + 1
+    runners = list(runners.values())
 
-    # runners = [int(sys.stdin.readline()) for _ in range(n)]
-    #
-    # sorted_runners = sorted(runners)
-    # runner_dic = {}
-    # for i, runner in enumerate(runners):
-    #     runner_dic[runner] = [None, i]
-    # for rank, runner in enumerate(sorted_runners):
-    #     runner_dic[runner][0] = rank
-    # rank_runners = []
-    # for runner in runners:
-    #     rank_runners.append(runner_dic[runner][0])
-    # print(runners)
-    # print(rank_runners)
-    # print()
-    # sorted_runners = []
-    #
-    # for i in range(n):
-    #     runners.append([None, s])   # [순위, 실력]
-    # sorted_runners = sorted(runners, key=lambda x: x[1])
-    # print(runners)
+    h = ceil(log2(n)) + 1 # n = 8 일때 h = 4, n = 9일때 h = 5
+    tree_size = 2 ** h - 1  # h = 4 일때 size = 15, h = 5일때 size = 31
+    tree = [None] + [0] * tree_size    # 인덱스는 1부터 시작
+    start_leaf = tree_size - n  # tree[start_leaf + num] -> 실제 num 값이 되도록
 
-
-    best_runner = max(runners)
-    h = ceil(log2(best_runner)) + 1
-    tree_size = 2 ** h - 1
-    tree = [0] * (tree_size + 1)
-    start_leaf = 2 ** (h - 1)
-
-    for runner in runners:
+    for i, runner in enumerate(runners):
+        print("*"*50)
         print(f"insert the {runner}")
+        print_tree()
+        passable_person = prefix_sum(1, n, 1, runner)
+        print(f"현재 등수 : {i + 1}  제낄만 한사람 : {passable_person}")
+        print(f"best rank is {i + 1 - passable_person}")
+        # print(i + 1 - passable_person)
         insert(runner)
         print_tree()
-
-
-
-    print()
 
