c2f1faf4ab08f72fda57762b83328afea7f62146
Jaewon Min <mjw8523@sju.ac.kr>
2021-09-07 22:43:46 +0900

    9/7

--- python/backjoon/트리/1167.py
+++ python/backjoon/트리/1167.py
@@ -11,16 +11,37 @@ import sys
 
 if __name__ == "__main__":
     v = int(sys.stdin.readline())
-
+    sys.setrecursionlimit(int(1e6))
     graph = {}
     for i in range(v):
         lists = list(map(int, sys.stdin.readline().split()))[:-1]
         ptr_node = lists[0]
         graph[ptr_node] = []
+        for i in range(1, len(lists), 2):
+            graph[ptr_node].append(tuple(lists[i:i+2]))
 
-        ptr_node
 
+    def find_farthest_node(current_node, diameter):
+        global max_diameter, farthest_node
+        visit[current_node] = True
+        is_leaf = True
+        for next_node, weight in graph[current_node]:
+            if not visit[next_node]:
+                is_leaf = False
+                find_farthest_node(next_node, diameter + weight)
+        visit[current_node] = False
+        if is_leaf and max_diameter < diameter:
+            max_diameter = diameter
+            farthest_node = current_node
 
-    print(graph)
+    visit = [False] * (v + 1)
+    max_diameter = 0
+    farthest_node = None
+    find_farthest_node(1, 0)
+
+    max_diameter = 0
+    find_farthest_node(farthest_node, 0)
+    print(max_diameter)
+
 
 
