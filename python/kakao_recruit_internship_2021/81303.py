0657fc99f06df9311268cb2ed798d85a2bb67e9d
Jaewon Min <mjw8523@sju.ac.kr>
2021-10-22 00:18:40 +0900

    10/22

--- python/kakao_recruit_internship_2021/81303.py
+++ python/kakao_recruit_internship_2021/81303.py
@@ -1,43 +1,83 @@
 """
 https://programmers.co.kr/learn/courses/30/lessons/81303
+현재 up 이랑 down 이랑 반대로 되어있음 여기부터 고쳐야함 ㅁㅅㅁ...
+
 """
 
 # from collections
 
-def cmd_up(row, step):
+def cmd_up(dic, row, step):
+    if step == 0:
+        return row
+    return cmd_up(dic, dic[row][1], step-1)
 
 
+def cmd_down(dic, row, step):
+    if step == 0:
+        return row
+    return cmd_down(dic, dic[row][0], step-1)
 
-def cmd_down():
-    ...
 
-
-def cmd_del():
+def cmd_del(dic, delete_log, row):
     # undo를 위해 delete log를 남겨둬야함
-    ...
+    delete_log.append(row)
+    next_row, before_row = dic[row]
+    if before_row == -1:    # 맨 처음 row 였을때
+        dic[row+1][0] = before_row
+        dic[row] = None
+        return next_row
+
+    elif next_row == len(dic):  # 맨 마지막 row 였을때
+        dic[row-1][1] = next_row
+        dic[row] = None
+        return before_row
+    else:
+        dic[row-1][1] = next_row
+        dic[row+1][0] = before_row
+        dic[row] = None
+        return next_row
 
 
-def cmd_undo():
-    ...
+def cmd_undo(dic, k, row):
+    next_row, before_row = row - 1, row + 1
+
+    while before_row >= 0:
+        if dic[before_row]:
+            break
+        before_row -= 1
+
+    while next_row < k:
+        if dic[next_row]:
+            break
+        next_row += 1
+
+    dic[row] = [before_row, next_row]
+
+    if before_row == -1 and next_row == k:
+        return
+    if before_row == -1:
+        dic[next_row][0] = row
+    if next_row == k:
+        dic[before_row][1] = row
 
 
 def solution(n, k, cmd):
     answer = ''
     delete_log = []
 
-    dic = {i: i+1 for i in range(n)}   # dic은 python 3.6부터 순서를 보장 set은 아직임
-    delete_set = set()
+    dic = {i: [i-1, i+1] for i in range(n)}   # dic은 python 3.6부터 순서를 보장 set은 아직임
+
 
     for query in cmd:
         if query[0] == "U":
-            k += int(query[-1])
+            k = cmd_up(dic, k, int(query[-1]))
         elif query[0] == "D":
-            k -= int(query[-1])
+            k = cmd_down(dic, k, int(query[-1]))
         elif query[0] == "C":
-            delete_log.append(k)
+            cmd_del(dic, delete_log, k)
 
         elif query[0] == "Z":
-            cmd_undo()
+            cmd_undo(dic, n, k)
     return answer
 
 
