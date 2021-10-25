6faa12e8673619bcc65f2912bc533df98581056c
Jaewon Min <mjw8523@sju.ac.kr>
2021-10-25 23:59:13 +0900

    10/25

--- python/test.py
+++ python/test.py
@@ -1,39 +1,56 @@
-#
-# class my_iter(object):
-#
-#     def __init__(self, _list: list):
-#         self.value = _list
-#
-#     def __iter__(self):
-#         self.iter_cnt = -2
-#         return self
-#
-#
-#     def __next__(self):
-#
-#         if self.iter_cnt < len(self.value):
-#             yield self.value[self.iter_cnt]
-#             self.iter_cnt += 2
-#             # return self.value[self.iter_cnt]
-#         else:
-#             raise StopIteration
-#
-# if __name__ == '__main__':
-#     a = list((1,2,3,4))
-#     print(type(a))
-#
-#     iter_a = my_iter(a)
-#     print(type(iter_a))
-#
-#     for num in iter_a:
-#         print(num)
+import random
+import threading
+import time
+from collections import ChainMap
+def working():
+    max([random.random() for i in range(50000000)])
 
-def func(a, **b):
-    print(a)
-    print(b)
+def foo():
+    global x
 
+    for i in range(100000000):
+        x += 1
+
+
+def bar():
+    global x
+
+
+    for i in range(100000000):
+        x -= 1
 
 if __name__ == '__main__':
-    dic = {'1': 3}
-    
-    print(*dic)
+
+    # start = time.time()
+    # working()
+    # working()
+    # end = time.time()
+    # print(f'{end - start}')
+    #
+    # start = time.time()
+    # threads = []
+    # for i in range(2):
+    #     threads.append(threading.Thread(target=working))
+    #     threads[-1].start()
+    #
+    # for t in threads:
+    #     t.join()
+    #
+    # end = time.time()
+    #
+    # print(f'{end - start}')
+
+    # multi thread test
+    # x = 0  # A shared value
+    #
+    #
+    #
+    # t1 = threading.Thread(target=foo)
+    # t2 = threading.Thread(target=bar)
+    # t1.start()
+    # t2.start()
+    # t1.join()
+    # t2.join()  # Wait for completion
+    #
+    # print(x)
+from collections import deque
