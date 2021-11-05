5024619ea0f5fd6cb773547ec4ae052a7db1c2a1
Jaewon Min <mjw8523@sju.ac.kr>
2021-11-05 13:31:56 +0900

    lv4

---
+++ python/programmers/LV4/1.py
@@ -0,0 +1,17 @@
+
+from itertools import permutations, product, combinations
+
+def find_weight(matrix_sizes, weights):
+    weights = []
+    for i in range(len(matrix_sizes) - 1):
+        w = matrix_sizes[i + 1]
+def solution(matrix_sizes):
+    answer = 0
+
+
+    return answer
+
+
+if __name__ == '__main__':
+    matrix_size = [[5,3], [3,10], [10,6]]
+    print(solution(matrix_size))
