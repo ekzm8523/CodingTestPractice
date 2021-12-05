2a2798da16d19fac7b4ad28a703bf86a7b6d894e
ekzm8523 <mjw8523@gmail.com>
2021-12-05 18:32:07 +0900

    12/5

---
+++ python/kakao_blind_2018/17679.py
@@ -0,0 +1,19 @@
+# https://programmers.co.kr/learn/courses/30/lessons/17679
+
+
+
+def solution(m, n, board):
+    answer = 0
+
+
+    return answer
+
+
+if __name__ == '__main__':
+    m, n = 4, 5
+    board = ["CCBDE", "AAADE", "AAABF", "CCBBF"]
+    print(solution(m, n, board))
+
+    m, n = 6, 6
+    board = ["TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ"]
+    print(solution(m, n, board))
