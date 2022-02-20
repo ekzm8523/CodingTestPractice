d96f919dd249bf6aab6d5a1701a7be9e8eee0f89
ekzm8523 <mjw8523@gmail.com>
2022-02-20 16:03:44 +0900

    kakao blind 2022 5

--- python/kakao_blind_2022/5-2.py
+++ python/kakao_blind_2022/5-2.py
@@ -1,48 +1,47 @@
-# # https://programmers.co.kr/learn/courses/30/lessons/92343
-# from collections import deque
-# # DFS(현재 노드 번호, 양의 수, 늑대의 수, 다음으로 방문할 수 있는 노드의 집합)
-#
-#
-#
-#
-# def solution(info, edges):
-#     size = len(info)
-#     answer = -1
-#     graph = [[] for _ in range(size)]
-#     for a, b in edges:
-#         graph[a].append(b)
-#
-#     def dfs(current_node, sheep_cnt, wolf_cnt, remain_node_set):
-#
-#         if info[current_node] == 0:  # 양일때
-#             remain_node_set.remove(current_node)
-#             sheep_cnt += 1ㅅ
-#         else:  # 늑대일때
-#             if wolf_cnt < sheep_cnt - 1:
-#                 wolf_cnt += 1
-#                 remain_node_set.remove(current_node)
-#
-#         for child_node in graph[current_node]:
-#             if info[child_node] == 0:
-#                 remain_node_set.remove(child_node)
-#                 dfs(child_node, )
-#
-#         # for child_node in graph[current_node]:
-#         #     if info[child_node] == 0: # 양일때
-#         #         remain_node_set.remove(child_node)
-#         #         dfs(child_node, )
-#         #     else:  # 늑대일때
-#         #         ...
-#
-#     dfs(0, 0, 0, set(range(len(info))))
-#
-#
-#
-# if __name__ == '__main__':
-#     input_info = [0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1]
-#     input_edges = [[0, 1], [1, 2], [1, 4], [0, 8], [8, 7], [9, 10], [9, 11], [4, 3], [6, 5], [4, 6], [8, 9]]
-#     print(solution(input_info, input_edges))
-#
-#     input_info = [0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0]
-#     input_edges = [[0, 1], [0, 2], [1, 3], [1, 4], [2, 5], [2, 6], [3, 7], [4, 8], [6, 9], [9, 10]]
-#     print(solution(input_info, input_edges))
+# https://programmers.co.kr/learn/courses/30/lessons/92343
+from collections import deque
+# DFS(현재 노드 번호, 양의 수, 늑대의 수, 다음으로 방문할 수 있는 노드의 집합)
+
+
+
+
+def solution(info, edges):
+    size = len(info)
+    answer = -1
+    graph = [[] for _ in range(size)]
+    for a, b in edges:
+        graph[a].append(b)
+
+    def dfs(current_node, sheep_cnt, wolf_cnt, candidate_node, visit):
+        nonlocal answer
+        sheep_cnt, wolf_cnt = sheep_cnt + (info[current_node] ^ 1), wolf_cnt + info[current_node]  # 양, 늑대 cnt update
+
+        if sheep_cnt <= wolf_cnt or visit[current_node]:
+            return
+        visit[current_node] = True
+        answer = max(answer, sheep_cnt)  # update
+
+        candidate_node.extend(graph[current_node])
+
+        for node in candidate_node:
+            dfs(
+                current_node=node,
+                sheep_cnt=sheep_cnt,
+                wolf_cnt=wolf_cnt,
+                candidate_node=[node for node in candidate_node if not visit[node] and node != current_node],
+                visit=visit.copy()
+            )
+
+    dfs(current_node=0, sheep_cnt=0, wolf_cnt=0, candidate_node=[], visit=[False for _ in info])
+    return answer
+
+
+if __name__ == '__main__':
+
+    input_info = [0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1]
+    input_edges = [[0, 1], [1, 2], [1, 4], [0, 8], [8, 7], [9, 10], [9, 11], [4, 3], [6, 5], [4, 6], [8, 9]]
+    print(solution(input_info, input_edges))
+
+    input_info = [0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0]
+    input_edges = [[0, 1], [0, 2], [1, 3], [1, 4], [2, 5], [2, 6], [3, 7], [4, 8], [6, 9], [9, 10]]
+    print(solution(input_info, input_edges))
